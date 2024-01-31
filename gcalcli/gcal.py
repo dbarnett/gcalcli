39ac680f8c92245b1a26fdac005d42620dac7669
Martin Dengler <martin@martindengler.com>
2024-01-31 08:22:22 -0500

    _calendar_color: use calendar color if event has no color

--- gcalcli/gcal.py
+++ gcalcli/gcal.py
@@ -293,6 +293,8 @@ class GoogleCalendarInterface:
             return ansi_codes[event['colorId']]
         elif cal.get('colorSpec', None):
             return cal['colorSpec']
+        elif cal.get('colorId', None):
+            return ansi_codes[cal['colorId']]
         elif cal['accessRole'] == self.ACCESS_OWNER:
             return self.options['color_owner']
         elif cal['accessRole'] == self.ACCESS_WRITER:
